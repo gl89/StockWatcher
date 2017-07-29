31879ed77940ee1189e25c04068f796b2a48f30f
gl89 <gloterena@gmail.com>
2017-07-29 01:00:24 -0400

    More progress

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -1,6 +1,8 @@
 package com.google.gwt.sample.stockwatcher.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -8,6 +10,11 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.user.client.Window;
+import java.util.ArrayList;
 
 public class StockWatcher implements EntryPoint {
 
@@ -17,6 +24,7 @@ public class StockWatcher implements EntryPoint {
   private TextBox newSymbolTextBox = new TextBox();
   private Button addStockButton = new Button("Add");
   private Label lastUpdatedLabel = new Label();
+  private ArrayList<String> stocks = new ArrayList<String>();
 
   /**
    * Entry point method.
@@ -43,6 +51,60 @@ public class StockWatcher implements EntryPoint {
     // Move cursor focus to the input box.
     newSymbolTextBox.setFocus(true);
 
-  }
+	//Listen for mouse events on the Add button.
+	addStockButton.addClickHandler(new ClickHandler() {
+	    public void onClick(ClickEvent event) {
+	      addStock();
+	    }
+	  });
+	
+  
+  // Listen for keyboard events in the input box.
+  newSymbolTextBox.addKeyDownHandler(new KeyDownHandler() {
+    public void onKeyDown(KeyDownEvent event) {
+      if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+        addStock();
+      }
+    }
+  });
+}
 
+	 /**
+	 * Add stock to FlexTable. Executed when the user clicks the addStockButton or
+	 * presses enter in the newSymbolTextBox.
+	 */
+	private void addStock() {
+	    final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
+	      newSymbolTextBox.setFocus(true);
+
+	      // Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
+	      if (!symbol.matches("^[0-9A-Z\\.]{1,10}$")) {
+	        Window.alert("'" + symbol + "' is not a valid symbol.");
+	        newSymbolTextBox.selectAll();
+	        return;
+	      }
+
+	      newSymbolTextBox.setText("");
+
+	      // TODO Don't add the stock if it's already in the table.
+	      
+	      if (stocks.contains(symbol))
+	          return;
+	      // TODO Add the stock to the table
+	      // Add the stock to the table.
+	      int row = stocksFlexTable.getRowCount();
+	      stocks.add(symbol);
+	      stocksFlexTable.setText(row, 0, symbol);
+	      // TODO Add a button to remove this stock from the table.
+	      Button removeStockButton = new Button("x");
+	      removeStockButton.addClickHandler(new ClickHandler() {
+	        public void onClick(ClickEvent event) {
+	          int removedIndex = stocks.indexOf(symbol);
+	          stocks.remove(removedIndex);
+	          stocksFlexTable.removeRow(removedIndex + 1);
+	        }
+	      });
+	      stocksFlexTable.setWidget(row, 3, removeStockButton);
+	      // TODO Get the stock price.
+	}
 }
